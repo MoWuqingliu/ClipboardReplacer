6fac956dec5605ad284f7f4292d9b6a7b508d09e
Ishiguro <Ishiguro@FIELD>
2023-05-01 20:47:02 +0800

    添加项目文件。

---
+++ ClipboardTool/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Text.RegularExpressions;
+
+
+namespace ClipboardTool
+{
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Clipboard.ContainsText())
+            {
+                MessageBox.Show("No text in Clipboard");
+                return;
+            }
+            var text = Clipboard.GetText();
+            this.text.Text = text;
+        }
+
+        private void Write_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(text.Text);
+            text.Text = "Write Sucessful";
+        }
+
+        private void Replace_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = Regex.Replace(text.Text, regex.Text, replace.Text);
+        }
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {   
+            if(text.Text.Length == 0)
+            {
+                MessageBox.Show("Emptpy String");
+                return;
+            }
+            QRCodeWindow window = new(text.Text);
+            window.ShowDialog();
+        }
+    }
+}
---
+++ ClipboardTool/QRCodeWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using QRCoder;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ClipboardTool
+{
+    /// <summary>
+    /// QRCode.xaml 的交互逻辑
+    /// </summary>
+    public partial class QRCodeWindow : Window
+    {
+        public QRCodeWindow(String value)
+        {
+            InitializeComponent();
+
+            using QRCodeGenerator qrGenerator = new();
+            using QRCodeData qrCodeData = qrGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+            using QRCode qrCode = new(qrCodeData);
+            var qrCodeBitMap = qrCode.GetGraphic(20);
+
+            MemoryStream stream = new();
+            qrCodeBitMap.Save(stream, ImageFormat.Bmp);
+
+            BitmapImage image = new();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+
+            img.Source = image;
+        }
+        private void CloseKeyDown(object sender,KeyEventArgs e)
+        {
+            if (e.KeyStates == Keyboard.GetKeyStates(Key.Q)
+                || e.KeyStates == Keyboard.GetKeyStates(Key.Space)
+                || e.KeyStates == Keyboard.GetKeyStates(Key.Enter))
+            {
+                DialogResult = true;
+            }
+
+        }
+
+    }
+
+
+}
